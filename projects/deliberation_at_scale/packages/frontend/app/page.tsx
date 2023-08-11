96f1a40857bc900e9250245d257f004af55b23d1
Jorim <jorim@dembrane.com>
2023-08-11 16:36:04 +0200

    style(deliberation-at-scale): added gifs to the webcam placeholder

--- projects/deliberation_at_scale/packages/frontend/app/page.tsx
+++ projects/deliberation_at_scale/packages/frontend/app/page.tsx
@@ -37,18 +37,18 @@ export default function Index() {
         <div className="grid grid-cols-1 grid-rows-2 md:grid-rows-1 md:grid-cols-2">
           <div className="h-[calc(50dvh-100px)] md:h-[calc(100dvh-110px)] grid gap-4 grid-cols-2 md:grid-rows-2 md:grid-cols-1 m-5 mb-0 md:mr-0 md:mb-5 relative">
             <img
-              src="https://media.istockphoto.com/id/1173667014/photo/happy-senior-retired-man-looking-talking-to-camera-webcam.jpg?s=612x612&w=0&k=20&c=p_r3UNUGoMh8wdQKIh1f4z80A_8QJDIvqaLHhh_Xy5U="
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2FIhLq8fGZw2SEE%2Fgiphy.gif&f=1&nofb=1&ipt=ccca2c7a9b973e3d349abfcb927b04f84d216d60bcf64579127443e5359181fe&ipo=images"
               alt=""
               className="object-cover object-center h-full w-full rounded"
             />
             <img
-              src="https://media.istockphoto.com/id/1189198083/photo/smiling-attractive-young-lady-looking-talking-to-camera-at-home.jpg?s=612x612&w=0&k=20&c=rQv-HePZ9th7bvdpIkK4Sxk3p_Q9LwNId5P2GiqJGc4="
+              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.tenor.com%2Fimages%2F885b334573ef7e48ea15cfbcc127e667%2Ftenor.gif%3Fitemid%3D13886831&f=1&nofb=1&ipt=6115b86a0b05782d6954bf9a405883ed1b9ec00359dcffbe253a36c9d76d5138&ipo=images"
               alt=""
               className="object-cover object-center h-full w-full rounded"
             />
-            <div className="absolute bottom-0 right-0 h-24 m-5">
+            <div className="absolute bottom-0 right-0 w-24 md:w-48 m-5">
               <img
-                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fas1.ftcdn.net%2Fjpg%2F03%2F66%2F07%2F94%2F220_F_366079479_AHlIEKMaEreWLM3aSTvS2Ff7i816EMiE.jpg&f=1&nofb=1&ipt=d5c26442b00c50c49c2c9eedc9da8ea97e2f1dd9e05d62da5018957296c25940&ipo=images"
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs3.amazonaws.com%2Fatiwl%2Fgifs%2F1521918972150-390a2opep2n-8cb695ff7402fbc2a574f8e64b2cace5%2Fblob.gif&f=1&nofb=1&ipt=ffceca88f898f2f3bbe0f790fa783e2720c98e0d31e1309238005259fe4ea64c&ipo=images"
                 alt=""
                 className="object-cover object-center h-full w-full rounded shadow"
               />
