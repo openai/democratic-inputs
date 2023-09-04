b864f8f0f8f0ea0eea3d6a4ab50bc74b749271e6
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-09-04 18:02:04 +0200

    fix(deliberation-at-scale): fixed issue with rerenders when showing user.

--- projects/deliberation_at_scale/packages/frontend/hooks/useAuth.ts
+++ projects/deliberation_at_scale/packages/frontend/hooks/useAuth.ts
@@ -1,14 +1,15 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { User } from "@supabase/gotrue-js";
 
-import { supabase } from "@/utilities/supabase";
+import { supabase } from "@/state/supabase";
 
 export default function useAuth() {
   const [user, setUser] = useState<User | null>(null);
-  const updateUser = async () => {
+  const updateUser = useCallback(async () => {
     const { data: { user } } = await supabase.auth.getUser();
+
     setUser(user);
-  };
+  }, []);
 
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
