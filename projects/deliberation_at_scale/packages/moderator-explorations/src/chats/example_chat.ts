2a7b93beb1ab4e20378bd96d89bf5c6fb3f3588e
Bart van Dijk <bart@moeilijkedingen.nl>
2023-09-06 13:45:52 +0200

    feat(deliberation-at-scale): moderator-explorations: added scaffolding for contribution and consensus moderation

--- projects/deliberation_at_scale/packages/moderator-explorations/src/chats/example_chat.ts
+++ projects/deliberation_at_scale/packages/moderator-explorations/src/chats/example_chat.ts
@@ -1,16 +1,16 @@
 import Message from '../types/message';
 
 const messages: Message[] = [
-    {"name": "John", "content": "Hey, I wanted to get your thoughts on something that's been bothering me lately. Have you heard about the new policy that students are not allowed to use AI technology for their exams?"},
-    {"name": "Sarah", "content": "Yeah, I've heard about it.”"},
-    // {"name": "John", "message": "Using AI could be seen as an unfair advantage."},
-    // {"name": "Mark", "message": "Allowing AI might lead to students relying too heavily on technology and not actually learning the material."},
-    // {"name": "Sarah", "message": "Exams are a way to evaluate our understanding of the subject matter. If we use AI for everything, how can we be sure that we've truly learned the material?"},
-    // {"name": "John", "message": "But isn't it also true that in the real world, we often use AI tools to assist us in making decisions and solving problems?"},
-    // {"name": "Mark", "message": "Not all students have access to the same level of AI technology."},
-    // {"name": "Sarah", "message": "I agree with Mark on this one. Some students might have access to more advanced AI tools, giving them an unfair advantage over others. That could lead to academic inequality."},
-    // {"name": "John", "message": "You're right, Sarah. That's a concern. But what if schools provided access to standardized AI tools for all students during exams? That way, everyone would have an equal opportunity."},
-    // {"name": "Mark", "message": "That's an interesting idea, John, but it would require a significant investment from schools, and not all institutions may be able to afford it. Plus, there's still the question of whether it's the right approach."}
+    {id: 0, "name": "John", "content": "Hey, I wanted to get your thoughts on something that's been bothering me lately. Have you heard about the new policy that students are not allowed to use AI technology for their exams?"},
+    {id: 1, "name": "Sarah", "content": "Yeah, I've heard about it.”"},
+    {id: 2, "name": "John", "content": "Using AI could be seen as an unfair advantage."},
+    {id: 3, "name": "Mark", "content": "Allowing AI might lead to students relying too heavily on technology and not actually learning the material."},
+    {id: 4, "name": "Sarah", "content": "Exams are a way to evaluate our understanding of the subject matter. If we use AI for everything, how can we be sure that we've truly learned the material?"},
+    {id: 5, "name": "John", "content": "But isn't it also true that in the real world, we often use AI tools to assist us in making decisions and solving problems?"},
+    {id: 6, "name": "Mark", "content": "Not all students have access to the same level of AI technology."},
+    {id: 7, "name": "Sarah", "content": "I agree with Mark on this one. Some students might have access to more advanced AI tools, giving them an unfair advantage over others. That could lead to academic inequality."},
+    {id: 8, "name": "John", "content": "You're right, Sarah. That's a concern. But what if schools provided access to standardized AI tools for all students during exams? That way, everyone would have an equal opportunity."},
+    {id: 9, "name": "Mark", "content": "That's an interesting idea, John, but it would require a significant investment from schools, and not all institutions may be able to afford it. Plus, there's still the question of whether it's the right approach."}
 ];
 
 export default messages;
--- projects/deliberation_at_scale/packages/moderator-explorations/src/main.ts
+++ projects/deliberation_at_scale/packages/moderator-explorations/src/main.ts
@@ -2,6 +2,8 @@ require("dotenv").config();
 import messages from './chats/example_chat';
 import Message from './types/message';
 import badLanguage from './moderators/bad-language';
+import consensus from './moderators/consensus';
+import equalContribution from './moderators/equal-contribution';
 
 async function main(messages: Message[], messagesCount: number) {
   const messagesForModeration = messages.slice(0, messagesCount);
@@ -15,9 +17,18 @@ async function main(messages: Message[], messagesCount: number) {
 }
 
 async function moderator(messages: Message[]) {
-  console.log('Run moderation for amount of messages: ', messages.length);
+  // BAD LANGUAGE
   const lastMessage = messages[messages.length - 1];
   // badLanguage(lastMessage);
+  
+  // CONSENSUS
+  // consensus(messages);
+
+  // CONTRIBUTION
+  const contributionCheckMessagesCount = 5; // Amount of messages that are used to compare the contribution between participants.
+  const contributionMessages = (messages.length <= contributionCheckMessagesCount) ? messages : messages.slice((messages.length - contributionCheckMessagesCount - 1), messages.length - 1);
+  equalContribution(contributionMessages)
+
 }
 
 main(messages, 1);
