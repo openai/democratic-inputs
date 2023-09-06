4b8611ccc221bc143c85dba61f84d17043a93bad
Bart van Dijk <bart@moeilijkedingen.nl>
2023-09-06 12:14:00 +0200

    feat(deliberation-at-scale): moderator-explorations: added scaffolding to loop through chat messages and example bad language checker.

---
+++ projects/deliberation_at_scale/packages/moderator-explorations/src/chats/example_chat.ts
@@ -0,0 +1,16 @@
+import Message from '../types/message';
+
+const messages: Message[] = [
+    {"name": "John", "content": "Hey, I wanted to get your thoughts on something that's been bothering me lately. Have you heard about the new policy that students are not allowed to use AI technology for their exams?"},
+    {"name": "Sarah", "content": "Yeah, I've heard about it.”"},
+    // {"name": "John", "message": "Using AI could be seen as an unfair advantage."},
+    // {"name": "Mark", "message": "Allowing AI might lead to students relying too heavily on technology and not actually learning the material."},
+    // {"name": "Sarah", "message": "Exams are a way to evaluate our understanding of the subject matter. If we use AI for everything, how can we be sure that we've truly learned the material?"},
+    // {"name": "John", "message": "But isn't it also true that in the real world, we often use AI tools to assist us in making decisions and solving problems?"},
+    // {"name": "Mark", "message": "Not all students have access to the same level of AI technology."},
+    // {"name": "Sarah", "message": "I agree with Mark on this one. Some students might have access to more advanced AI tools, giving them an unfair advantage over others. That could lead to academic inequality."},
+    // {"name": "John", "message": "You're right, Sarah. That's a concern. But what if schools provided access to standardized AI tools for all students during exams? That way, everyone would have an equal opportunity."},
+    // {"name": "Mark", "message": "That's an interesting idea, John, but it would require a significant investment from schools, and not all institutions may be able to afford it. Plus, there's still the question of whether it's the right approach."}
+];
+
+export default messages;
