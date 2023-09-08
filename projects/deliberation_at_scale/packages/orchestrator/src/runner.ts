1f0c5b3b4c0ba93db1ec17df8f8c0cda2c37021d
Bart van Dijk <bart@moeilijkedingen.nl>
2023-09-08 14:10:03 +0200

    feat(deliberation-at-scale): moderator-explorations: starting scheduler from runner

--- projects/deliberation_at_scale/packages/orchestrator/src/runner.ts
+++ projects/deliberation_at_scale/packages/orchestrator/src/runner.ts
@@ -2,6 +2,7 @@ require("dotenv").config()
 import { parseCronItems, run, quickAddJob } from "graphile-worker"
 import crontab from "./crontab"
 import supabase from "./lib/supabase"
+import startScheduler from './scheduler'
 
 async function main() {
   const runner = await run({
@@ -53,6 +54,11 @@ listenForNewMessages().catch((err) => {
 })
 
 main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
+
+startScheduler().catch((err) => {
   console.error(err)
   process.exit(1)
 })
