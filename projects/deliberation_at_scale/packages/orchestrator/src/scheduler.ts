10544a69560c5e358ef4174f89bebea9f6647b1d
Bart van Dijk <bart@moeilijkedingen.nl>
2023-09-08 14:10:03 +0200

    feat(deliberation-at-scale): moderator-explorations: starting scheduler from runner

--- projects/deliberation_at_scale/packages/orchestrator/src/scheduler.ts
+++ projects/deliberation_at_scale/packages/orchestrator/src/scheduler.ts
@@ -2,7 +2,7 @@
 require('dotenv').config();
 import { quickAddJob } from "graphile-worker";
 
-async function main() {
+async function startScheduler() {
     quickAddJob({}, "summarize", 0, {
         jobKey: "summarize",
         jobKeyMode: "preserve_run_at",
@@ -11,7 +11,4 @@ async function main() {
     // for every message from supabase, quickAdd job the moderation task with the message as payload
 }
 
-main().catch((err) => {
-    console.error(err);
-    process.exit(1);
-});
+export default startScheduler;
