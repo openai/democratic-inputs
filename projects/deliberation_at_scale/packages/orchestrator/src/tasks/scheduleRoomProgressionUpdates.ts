afcdf98ea5c80f6e1a28c16e801eeeac8a486c55
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-09-15 14:26:25 +0200

    chore(deliberation-at-scale): minor formatting upgrades.

--- projects/deliberation_at_scale/packages/orchestrator/src/tasks/scheduleRoomProgressionUpdates.ts
+++ projects/deliberation_at_scale/packages/orchestrator/src/tasks/scheduleRoomProgressionUpdates.ts
@@ -1,16 +1,21 @@
 import { Helpers } from "graphile-worker";
 
-import supabaseClient from "../lib/supabase";
+import { supabaseClient } from "../lib/supabase";
 import { UpdateRoomProgressionPayload } from "./updateRoomProgression";
 
 export interface ScheduleRoomProgressionUpdatesPayload {
 }
+
 /**
  * This task schedules progression update tasks for all active rooms.
  * This means individual runners can take onto one room, because a progression check is quite an heavy task.
  */
 export default async function scheduleRoomProgressionUpdates(payload: ScheduleRoomProgressionUpdatesPayload, helpers: Helpers) {
-    const activeRoomsData = await supabaseClient.from('rooms').select().eq('active', true).not('starts_at', 'is', null);
+    const activeRoomsData = await supabaseClient
+        .from('rooms')
+        .select()
+        .eq('active', true)
+        .not('starts_at', 'is', null);
     const activeRooms = activeRoomsData?.data ?? [];
     const activeRoomsAmount = activeRooms.length;
 
