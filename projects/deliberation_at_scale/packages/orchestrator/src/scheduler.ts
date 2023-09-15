9a14a810a3858da15db45491bb1d4da87925ee52
Emma Driesse <emma@moeilijkedingen.nl>
2023-09-15 17:22:38 +0200

    feat(deliberation-at-scale):moderations: added verifyOffTopic task an verifyIntroductionParticipants task

--- projects/deliberation_at_scale/packages/orchestrator/src/scheduler.ts
+++ projects/deliberation_at_scale/packages/orchestrator/src/scheduler.ts
@@ -1,18 +1,27 @@
 import { quickAddJob } from "graphile-worker";
 
 export async function startScheduler() {
-    quickAddJob({}, "scheduleRoomProgressionUpdates", 0, {
-        jobKey: "scheduleRoomProgressionUpdates",
-        jobKeyMode: "preserve_run_at",
-    });
+    // quickAddJob({}, "scheduleRoomProgressionUpdates", 0, {
+    //     jobKey: "scheduleRoomProgressionUpdates",
+    //     jobKeyMode: "preserve_run_at",
+    // });
 
-    // TMP: for testing
-    quickAddJob({}, "difficultLanguage", 0, {
-        jobKey: "difficultLanguage",
-        jobKeyMode: "preserve_run_at",
-    });
-    quickAddJob({}, "equalParticipation_2", 0, {
-        jobKey: "equalParticipation_2",
+    // // TMP: for testing
+    // quickAddJob({}, "difficultLanguage", 0, {
+    //     jobKey: "difficultLanguage",
+    //     jobKeyMode: "preserve_run_at",
+    // });
+    // quickAddJob({}, "equalParticipation_2", 0, {
+    //     jobKey: "equalParticipation_2",
+    //     jobKeyMode: "preserve_run_at",
+    // });
+
+    // quickAddJob({}, "verifyConsensusForming", 0, {
+    //     jobKey: "verifyConsensusForming",
+    //     jobKeyMode: "preserve_run_at",
+    // });
+    quickAddJob({}, "verifyEqualParticipation", 0, {
+        jobKey: "verifyEqualParticipation",
         jobKeyMode: "preserve_run_at",
     });
 }
--- projects/deliberation_at_scale/packages/orchestrator/src/tasks/verifyIntroductionParticipants.ts
+++ projects/deliberation_at_scale/packages/orchestrator/src/tasks/verifyIntroductionParticipants.ts
@@ -1,3 +1,122 @@
-export default function introductionParticipants() {
-    
+import { Helpers, quickAddJob } from "graphile-worker";
+import supabaseClient, { getTopic, selectMessages } from "../lib/supabase";
+import openaiClient, { createVerificationFunctionCompletion } from "../lib/openai";
+import { CreateChatCompletionRequestMessage } from "openai/resources/chat";
+import { Database } from "../generated/database.types";
+
+type Message = Database["public"]["Tables"]["messages"]["Row"]
+
+/**
+ * Run this task at most every n seconds
+ */
+const TASK_INTERVAL_SECONDS = 10;
+const historyAmountMessages = 10;
+
+/**
+ * This task retrieves all messages since its last execution and attempts to
+ * summarize them using GPT.
+ */
+export default async function verifyIntroductionParticipants(
+    lastRun: string | null,
+    helpers: Helpers
+) {  
+
+    // Retrieve all messages from supabase
+    const messages = await selectMessages({
+        historyAmountMessages,
+    });
+
+    // GUARD: If there are no messages, reschedule the job
+
+    if (messages === null) {
+        helpers.logger.info("Mesassages undefined.");
+        return reschedule(lastRun);
+    }
+
+    if (messages.length === 0) {
+        helpers.logger.info("No messages found.");
+        return reschedule(lastRun);
+    }
+
+    const { id: messageId, content, room_id: roomId, } = messages[0] ?? {};
+
+    const verificationResult = await createVerificationFunctionCompletion({
+        taskInstruction: `
+        Three participants are introducing themselves to each other.
+
+        Have all participants finished introducing themselves??
+
+        `,
+        //verified,
+        // not difficult example
+        taskContent:    
+        
+        JSON.stringify(
+            messages.map(
+                (message) =>
+                    ({
+                        participant: message.participant_id,
+                        content: message.content,
+                    })
+            )
+        )
+    });
+    const isAllIntroduced = verificationResult.verified;
+    const allIntroducedReason = verificationResult.reason;
+
+    // TMP: temporary logging statements
+    console.log('Equal participation verification result:');
+    console.log(isAllIntroduced);
+
+    console.log('Reason:');
+    console.log(allIntroducedReason);
+
+    // guard: do nothing when it is not difficult language
+    if (isEqualParticipation || !roomId) {
+        return;
+    }
+
+    helpers.logger.info(`Sending clarification message to room ${roomId}: ${allIntroducedReason}`);
+
+    // execute these in parallel to each other
+    await Promise.allSettled([
+
+        // track that this message has been moderated
+        insertModeration(messages, equalParticipationReason),
+
+        // send a message to the room with the moderators explaination about the verification
+        // sendBotMessage({
+        //     content: unappropiateReasonToParticipants,
+        //     roomId,
+        // }),
+    ]);
+
+
+    return reschedule(lastRun);
+}
+
+/**
+ * Reschedule this job for the next iteration
+ */
+function reschedule(initialDate: string | null) {
+    // Re-schedule this job n seconds after the last invocation
+    quickAddJob({}, "verifyIntroductionParticipants", new Date(), {
+        runAt: new Date(
+            (initialDate ? new Date(initialDate) : new Date()).getTime() +
+      1_000 * TASK_INTERVAL_SECONDS
+        ),
+        jobKey: "verifyIntroductionParticipants",
+        jobKeyMode: "preserve_run_at",
+    });
+}
+
+async function insertModeration(message: Message, statement: string) {
+    await supabaseClient.from("moderations").insert({
+        type: 'verifyIntroductionParticipants',
+        statement,
+        target_type: 'message',
+        message_id: message.id,
+        participant_id: message.participant_id,
+        room_id: message.room_id,
+    });
 }
