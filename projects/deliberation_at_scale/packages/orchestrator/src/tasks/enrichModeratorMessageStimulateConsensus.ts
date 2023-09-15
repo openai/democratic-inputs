3edec85c8602e31676f092fa5c53e609669f84ef
Eva van der Born <43637609+evavdborn@users.noreply.github.com>
2023-09-15 16:11:47 +0200

    feat(deliberation-at-scale): moderators: implemented enrichmentFunction and tested it with enrichModeratorMessageStimulateConsensus

--- projects/deliberation_at_scale/packages/orchestrator/src/tasks/enrichModeratorMessageStimulateConsensus.ts
+++ projects/deliberation_at_scale/packages/orchestrator/src/tasks/enrichModeratorMessageStimulateConsensus.ts
@@ -1,82 +1,70 @@
-import { Helpers, quickAddJob } from "graphile-worker";
-import supabaseClient, { Message, sendBotMessage, selectMessages } from "../lib/supabase";
-import openaiClient from "../lib/openai";
-
-const historyAmountSeconds = 30;
-
-export interface enrichModeratorMessageStimulateConsensusPayload {
-    message: Message;
-    lastRun: string | null,
-    helpers: Helpers
-    // roomId: string;
-    // messageId: string;
-
-}
+import { Helpers } from "graphile-worker";
+import supabaseClient, { Message, sendBotMessage } from "../lib/supabase";
+import { createEnrichFunctionCompletion } from "../lib/openai";
 
 /**
- * This task sends out an enrichtment about consensus forming
+ * This task determines whether a single message is using very difficult language.
+ * If so, this message is flagged and an informal message from the moderator is sent out.
  */
-
-export default async function enrichModeratorMessageStimulateConsensus(payload: enrichModeratorMessageStimulateConsensusPayload, helpers: Helpers) {
-    const { message, lastRun } = payload;
-
-    console.log("ik ben hier");
-    // Retrieve messages from supabase
-    const messages = await selectMessages(historyAmountSeconds);
-
-    // GUARD: If there are no messages, reschedule the job
-    if (messages != null) {
-        if (messages.length === 0 ) {
-            helpers.logger.info("No messages found.");
-            return reschedule(lastRun);
-        }
-    }
+export default async function difficultLanguage(message: Message, helpers: Helpers) {
+    const { id: messageId, content, room_id: roomId } = message ?? {};
 
     // TODO: replace the task content with the one of the actual message
-    const enrichment = await openaiClient.completions.create({
-        temperature: 0.8,
-        messages: [
-            {
-                role: 'user',
-                content: `You are a moderator of a discussion between 3 participants.
-                In the discussion, no consensus between the participants has been found yet, because of the following reasons:
-                No consensus has been found because
-                Formulate a message of max. 20 words to guide the discussion towards a consensus
-                Message: ${message.content}
-            `},
-            ...messages.map(
-                (message) =>
-                    ({
-                        role: "user",
-                        content: message.content,
-                    } as CreateChatCompletionRequestMessage)
-            ),
-        ],
-        model: 'gpt-4',
+    const enrichResult = await createEnrichFunctionCompletion({
+        taskInstruction: `
+            You are a moderator of a discussion between 3 participants.
+            In the discussion, no consensus between the participants has been found yet, because of the following reasons:
+            No consensus has been found because
+            Formulate a message of max. 20 words to guide the discussion towards a consensus
+        `,
+        //verified,
+        // not difficult example
+        // taskContent: `
+        //     I hope I'm speaking clearly to you because my opinion is not only very valid, but I think you might misunderstand me.
+        // `,
+
+        // difficult example
+        taskContent: `
+            I think you are trying to be too extravagant with your outragous opinions, if you would be a fine sir I would've thought differently of you!
+        `,
+
+        // actual message content
+        // taskContent: content,
     });
 
-    console.log(enrichment);
+    const clarificationReasonToParticipants = enrichResult.enriched;
 
-    const enrichtmentResult = enrichment.choices[0];
+    // TMP: temporary logging statements
+    console.log('Enrich result:');
+    console.log(enrichResult);
 
-    if (!enrichtmentResult) {
-        console.log('No enrichtment');
-        return null;
+    // guard: do nothing when it is not difficult language
+    if (isUnderstandableLanguage || !roomId) {
+        return;
     }
 
-    return enrichtmentResult;
+
+    helpers.logger.info(`Sending clarification message to room ${roomId} for message ${messageId}: ${clarificationReasonToParticipants}`);
+
+    // execute these in parallel to each other
+    await Promise.allSettled([
+        // track that this message has been moderated
+        // send a message to the room with the moderators explaination about the verification
+        sendBotMessage({
+            content: clarificationReasonToParticipants,
+            roomId,
+        }),
+    ]);
+
 }
 
-const TASK_INTERVAL_SECONDS = 30;
-
-function reschedule(initialDate: string | null) {
-    // Re-schedule this job n seconds after the last invocation
-    quickAddJob({}, "enoughContent", new Date(), {
-        runAt: new Date(
-            (initialDate ? new Date(initialDate) : new Date()).getTime() +
-      1_000 * TASK_INTERVAL_SECONDS
-        ),
-        jobKey: "enoughContent",
-        jobKeyMode: "preserve_run_at",
-    });
-}
+// async function insertClarification(message: Message, statement: string) {
+//     await supabaseClient.from("moderations").insert({
+//         type: 'clarification',
+//         statement,
+//         target_type: 'message',
+//         message_id: message.id,
+//         participant_id: message.participant_id,
+//         room_id: message.room_id,
+//     });
+// }
