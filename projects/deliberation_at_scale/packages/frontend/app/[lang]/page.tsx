1e6498add57718b28e97220da72d330fae7e4923
Lei Nelissen <lei@codified.nl>
2023-09-27 10:32:32 -0700

    feat: add starting page graphic

--- projects/deliberation_at_scale/packages/frontend/app/[lang]/page.tsx
+++ projects/deliberation_at_scale/packages/frontend/app/[lang]/page.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 import Button from "@/components/Button";
 import useProfile from "@/hooks/useProfile";
-import { faArrowRightFromBracket, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightFromBracket, faRotate, faSpinner, faVideo, faMessage, faHandshake } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { supabaseClient } from '@/state/supabase';
 
@@ -20,10 +20,30 @@ export default function Index() {
     const logout = useCallback(() => supabaseClient.auth.signOut(), []);
 
     return (
-        <section className="m-4 flex flex-col justify-center gap-3">
+        <section className="m-4 flex flex-col justify-center gap-3 py-4">
             <motion.div layout>
-                <h1>Welcome,</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ipsum nec odio vestibulum varius. Fusce efficitur, dui id laoreet eleifend, sapien tortor venenatis quam, vel tincidunt metus sapien eget elit. Integer </p>
+                <h1 className="mb-4 text-lg font-medium">Welcome to Deliberation at Scale</h1>
+                <p className="mb-4 text-gray-600">The development of artificial intelligence (AI) affects all of us. With Deliberation at Scale, we want to ensure that guidelines and rules that will come to guide artifical intelligence development are <span className="font-semibold">well-sourced</span>, <span className="font-semibold">representative</span> and <span className="font-semibold">fair</span>.</p>
+                <p className="mb-4 text-gray-600">In Deliberation at Scale you will:</p>
+                <div className="grid grid-cols-2 grid-rows-2 gap-2 mb-4 hyphens-auto text-green-700 text-lg font-medium text-center">
+                    <div className="bg-green-50 p-4 py-6 rounded flex flex-col items-center gap-4">
+                        <FontAwesomeIcon icon={faVideo} size="2x" fixedWidth />
+                        <span>Meet two other participants in a video call</span>
+                    </div>
+                    <div className="bg-green-50 p-4 py-6 rounded flex flex-col items-center gap-4">
+                        <FontAwesomeIcon icon={faMessage} size="2x" fixedWidth />
+                        <span>Discuss topics surrounding AI governance</span>
+                    </div>
+                    <div className="bg-green-50 p-4 py-6 rounded flex flex-col items-center gap-4">
+                        <FontAwesomeIcon icon={faHandshake} size="2x" fixedWidth />
+                        <span>Agree on statements capturing a topic</span>
+                    </div>
+                    <div className="bg-green-50 p-4 py-6 rounded flex flex-col items-center gap-4">
+                        <FontAwesomeIcon icon={faRotate} size="2x" fixedWidth />
+                        <span>Contribute to a large body of agreement</span>
+                    </div>
+                </div>
+                <p className="mb-4 text-gray-600">Are you ready to help create guidelines for the next generation of artificial intelligence?</p>
             </motion.div>
             <motion.div className="flex flex-col gap-2">
                 {loading ? (
