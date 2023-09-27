8aea398e78a44ebf50df5a71adf960100dde5bea
Lei Nelissen <lei@codified.nl>
2023-09-27 09:39:09 -0700

    feat: add logout button and only show login buttons when logged out

--- projects/deliberation_at_scale/packages/frontend/app/[lang]/page.tsx
+++ projects/deliberation_at_scale/packages/frontend/app/[lang]/page.tsx
@@ -6,14 +6,18 @@ import { motion } from "framer-motion";
 
 import Button from "@/components/Button";
 import useProfile from "@/hooks/useProfile";
+import { faArrowRightFromBracket, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { supabaseClient } from '@/state/supabase';
 
 export default function Index() {
     const { push } = useRouter();
-    const { authUser } = useProfile();
+    const { authUser, loading } = useProfile();
     const isLoggedIn = !!authUser;
     const goToRegister = useCallback(() => push('/register'), [push]);
     const goToLogin = useCallback(() => push('/login'), [push]);
     const goToProfile = useCallback(() => push('/profile'), [push]);
+    const logout = useCallback(() => supabaseClient.auth.signOut(), []);
 
     return (
         <section className="m-4 flex flex-col justify-center gap-3">
@@ -22,13 +26,20 @@ export default function Index() {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ipsum nec odio vestibulum varius. Fusce efficitur, dui id laoreet eleifend, sapien tortor venenatis quam, vel tincidunt metus sapien eget elit. Integer </p>
             </motion.div>
             <motion.div className="flex flex-col gap-2">
-                {isLoggedIn ? (
-                    <Button icon={profileIcon} onClick={goToProfile}>Go to profile</Button>
+                {loading ? (
+                    <FontAwesomeIcon icon={faSpinner} spin className="text-gray-400" />
                 ) : (
-                    <>
-                        <Button icon={registerIcon} onClick={goToRegister}>Register</Button>
-                        <Button icon={loginIcon} onClick={goToLogin}>Login</Button>
-                    </>
+                    isLoggedIn ? (
+                        <>
+                            <Button icon={profileIcon} onClick={goToProfile}>Go to profile</Button>
+                            <Button icon={faArrowRightFromBracket} onClick={logout}>Log out</Button>
+                        </>
+                    ) : (
+                        <>
+                            <Button icon={registerIcon} onClick={goToRegister}>Register</Button>
+                            <Button icon={loginIcon} onClick={goToLogin}>Login</Button>
+                        </>
+                    )
                 )}
             </motion.div>
         </section>
