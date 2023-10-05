c00a9b543fddbfd9dc2dc8413bea98d3584a6c11
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-10-05 22:09:24 +0200

    fix(das): added check to leave queue when participant to ping is not valid anymore.

--- projects/deliberation_at_scale/packages/frontend/hooks/usePingParticipant.ts
+++ projects/deliberation_at_scale/packages/frontend/hooks/usePingParticipant.ts
@@ -1,35 +1,45 @@
 import dayjs from "dayjs";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 import { FullParticipantFragment, ParticipantStatusType, usePingParticipantMutation } from "@/generated/graphql";
 import { PARTICIPANT_PING_INTERVAL_DELAY_MS } from "@/utilities/constants";
 
 export function usePingParticipant(candidateParticipant?: FullParticipantFragment) {
     const participantId = candidateParticipant?.id;
+    const participantActive = candidateParticipant?.active;
     const participantStatus = candidateParticipant?.status;
     const [ping, { loading, error, data }] = usePingParticipantMutation();
+    const { push } = useRouter();
 
     useEffect(() => {
 
         // guard check if the ID is valid and only ping when queued
         // this prevent in confirmation participants to be updated as well
-        if (!participantId || participantStatus !== ParticipantStatusType.Queued) {
+        if (!participantId || participantStatus !== ParticipantStatusType.Queued || participantActive !== true) {
             return;
         }
 
-        const pingInterval = setInterval(() => {
-            ping({
+        const pingInterval = setInterval(async () => {
+            const pingResult = await ping({
                 variables: {
                     participantId,
                     lastSeenAt: dayjs().toISOString(),
                 }
             });
+
+            const affectedCount = pingResult.data?.updateparticipantsCollection?.affectedCount ?? 0;
+
+            // reload the pge when the participant got deactivated somehow
+            if (affectedCount <= 0) {
+                // document.location.reload();
+            }
         }, PARTICIPANT_PING_INTERVAL_DELAY_MS);
 
         return (() => {
             clearInterval(pingInterval);
         });
-    }, [participantId, participantStatus, ping]);
+    }, [participantId, participantStatus, participantActive, ping, push]);
 
     return { loading, error, data };
 }
