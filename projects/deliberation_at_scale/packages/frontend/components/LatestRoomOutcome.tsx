ccf7ac7b63b2e28928c574b868e58b0b0a6e1172
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-10-11 00:26:30 +0200

    feat(das): added proper motion design when hiding and showing outcomes.

--- projects/deliberation_at_scale/packages/frontend/components/LatestRoomOutcome.tsx
+++ projects/deliberation_at_scale/packages/frontend/components/LatestRoomOutcome.tsx
@@ -1,24 +1,29 @@
 'use client';
 import useRoom from '@/hooks/useRoom';
 import RoomOutcome from './RoomOutcome';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function LatestRoomOutcome() {
     const { outcomes, participantId } = useRoom();
     const lastOutcome = outcomes?.[0];
+    const hasOutcome = !!lastOutcome;
     const { id: outcomeId } = lastOutcome ?? {};
-
-    if (!lastOutcome) {
-        return null;
-    }
 
     return (
         <motion.div
-            initial={{ opacity: 0, x: -50 }}
-            animate={{ opacity: 1, x: 0 }}
-            className='mx-2 md:mx:4 my-2 p-4 border shadow rounded'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, height: hasOutcome ? 'auto' : 0 }}
         >
-            <RoomOutcome variant="compact" key={outcomeId} outcome={lastOutcome} participantId={participantId} />
+            <AnimatePresence>
+                {hasOutcome && (
+                    <motion.div
+                        exit={{ opacity: 0, y: -40 }}
+                        className='mx-2 md:mx-4 my-2 p-4 border shadow rounded'
+                    >
+                        <RoomOutcome variant="compact" key={outcomeId} outcome={lastOutcome} participantId={participantId} />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </motion.div>
     );
 }
