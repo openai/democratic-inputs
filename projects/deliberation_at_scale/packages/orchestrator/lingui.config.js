dd220a0e9a265affcc4537ea244b3a7f9e329760
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-10-11 22:59:57 +0200

    feat(das): added full support for localisation in the orchestrator. It is worth noting that for every language currently an orchestrator needs to be spun up. This is something that we can consider changing in the future.

---
+++ projects/deliberation_at_scale/packages/orchestrator/lingui.config.js
@@ -0,0 +1,19 @@
+const { locales, defaultLocale } = require('./locales');
+
+/** @type {import('@lingui/conf').LinguiConfig} */
+module.exports = {
+    locales,
+    pseudoLocale: 'pseudo',
+    sourceLocale: defaultLocale,
+    fallbackLocales: {
+        default: 'en'
+    },
+    catalogs: [
+        {
+            path: 'locales/{locale}',
+            include: [
+                './src/',
+            ]
+        }
+    ]
+};
