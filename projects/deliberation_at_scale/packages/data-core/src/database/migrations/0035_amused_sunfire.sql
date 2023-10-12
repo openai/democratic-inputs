b0a279f411eae8e00f2783fa357325b2b2cf3ed9
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-10-12 11:10:32 +0200

    feat(das): added types for ping participant RPC.

--- projects/deliberation_at_scale/packages/data-core/src/database/migrations/0035_amused_sunfire.sql
+++ projects/deliberation_at_scale/packages/data-core/src/database/migrations/0035_amused_sunfire.sql
@@ -1,2 +1,10 @@
 ALTER TABLE "participants" ALTER COLUMN "nick_name" SET DEFAULT 'Contributor';--> statement-breakpoint
 ALTER TABLE "users" ALTER COLUMN "nick_name" SET DEFAULT 'Contributor';
+
+CREATE OR REPLACE FUNCTION public.ping_participant(participant_id uuid) RETURNS uuid AS $$
+	DECLARE updated_id uuid;
+	BEGIN
+		UPDATE participants SET last_seen_at = NOW() WHERE id = participant_id RETURNING id INTO updated_id;
+	RETURN updated_id;
+	END;
+$$ LANGUAGE plpgsql;
