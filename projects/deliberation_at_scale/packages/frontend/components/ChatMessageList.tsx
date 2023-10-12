c9be64666b8b415f88a2dfc5d5990f2ef9420a56
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-10-12 15:20:34 +0200

    fix(das): temporarily disabled the fading scroll shadow. Can be enabled later again.

--- projects/deliberation_at_scale/packages/frontend/components/ChatMessageList.tsx
+++ projects/deliberation_at_scale/packages/frontend/components/ChatMessageList.tsx
@@ -14,38 +14,6 @@ interface Props {
 
 export default function ChatMessageList(props: Props) {
     const { messages, className } = props;
-    // state to control visibility of gradient shadows 
-    const [topShadow, setTopShadow] = useState(0);
-    //const [bottomShadow, setBottomShadow] = useState(0);
-
-    const ref = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        const current = ref.current;
-        const handleScroll = () => {
-            if (current) {
-                const scrollTop = current.scrollTop;
-                //const offsetHeight = current.offsetHeight;
-                //const scrollHeight = current.scrollHeight;
-                // Calculate opacity based on scroll position
-                const topShadowOpacity = Math.min(scrollTop / 100, 1);
-                //const bottomShadowOpacity = Math.min((scrollHeight - scrollTop - offsetHeight) / 100, 1);
-                // Set shadow visibility based on scroll position
-                setTopShadow(topShadowOpacity);
-                //setBottomShadow(bottomShadowOpacity);
-            }
-        };
-        if (current) {
-            current.addEventListener("scroll", handleScroll);
-        }
-        return () => {
-            if (current) {
-                current.removeEventListener("scroll", handleScroll);
-            }
-        };
-    }, []);
-
-
     const variants = {
         visible: {
             transition: {
@@ -67,54 +35,46 @@ export default function ChatMessageList(props: Props) {
         return null;
     }
 
-    
-
+    // <div className="min-h-[12px] md:min-h-[50px] w-full top-0 bg-gradient-to-t from-transparent to-white"></div>
     return (
-        <>
-            <div className="min-h-[12px] md:min-h-[50px] absolute w-full top-0 bg-gradient-to-t from-transparent to-white" style={{opacity: topShadow}}></div>
+        <motion.div
+            id={MESSAGES_SCROLL_CONTAINER_ID}
+            className={classNames(
+                "flex flex-col gap-1 overflow-y-scroll min-h-0 shrink",
+                className
+            )}
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+        >
+            <AnimatePresence>
+                {messages.map((message, index) => {
+                    const { id = index, date, nameId } = message ?? {};
+                    const key = `${id}-${date}`;
+                    const previousMessage = messages?.[index - 1];
+                    const nextMessage = messages?.[index + 1];
+                    const nameKey = nameId ? 'nameId' : 'name';
 
-            <motion.div
-                id={MESSAGES_SCROLL_CONTAINER_ID}
-                className={classNames(
-                    "flex flex-col gap-1 overflow-y-scroll min-h-0 shrink relative",
-                    className
-                )}
-                variants={variants}
-                initial="hidden"
-                animate="visible"
-                ref={ref}
-            >
-                <AnimatePresence>
-                
-                    {messages.map((message, index) => {
-                        const { id = index, date, nameId } = message ?? {};
-                        const key = `${id}-${date}`;
-                        const previousMessage = messages?.[index - 1];
-                        const nextMessage = messages?.[index + 1];
-                        const nameKey = nameId ? 'nameId' : 'name';
+                    // guard: check if message is valid
+                    if (!message) {
+                        return null;
+                    }
 
-                        // guard: check if message is valid
-                        if (!message) {
-                            return null;
-                        }
+                    const isFirst = previousMessage?.[nameKey] !== message?.[nameKey];
+                    const isLast = nextMessage?.[nameKey] !== message?.[nameKey];
 
-                        const isFirst = previousMessage?.[nameKey] !== message?.[nameKey];
-                        const isLast = nextMessage?.[nameKey] !== message?.[nameKey];
+                    return (
+                        <ChatMessage
+                            className={index === 0 ? 'mt-auto' : ''}
+                            key={key}
+                            message={message}
+                            first={isFirst}
+                            last={isLast}
+                        />
+                    );
+                })}
+            </AnimatePresence>
 
-                        return (
-                            <ChatMessage
-                                className={index === 0 ? 'mt-auto' : ''}
-                                key={key}
-                                message={message}
-                                first={isFirst}
-                                last={isLast}
-                            />
-                        );
-                    })}
-                </AnimatePresence>
-            
-            </motion.div>
-            {/* <div className="min-h-[12px] absolute w-full bottom-[0] bg-gradient-to-b from-transparent to-white" style={{opacity: bottomShadow}}></div> not working out with the variable height of the elements below it sadly  */}
-        </>
+        </motion.div>
     );
 }
