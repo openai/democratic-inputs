f453d1dbcc36ad6859e779c11d97fc963a75517b
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-10-15 23:10:19 +0200

    feat(das): added 'next statement' button when everyone has voted.

--- projects/deliberation_at_scale/packages/frontend/hooks/useRoomActions.ts
+++ projects/deliberation_at_scale/packages/frontend/hooks/useRoomActions.ts
@@ -1,13 +1,14 @@
 import { useAppSelector } from "@/state/store";
 import useRoom from "./useRoom";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import dayjs from "dayjs";
-import { OutcomeType } from "@/generated/graphql";
+import { OutcomeType, useSendRoomMessageMutation } from "@/generated/graphql";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { useLingui } from "@lingui/react";
 import { msg } from "@lingui/macro";
-import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
+import { faDoorOpen, faForward } from "@fortawesome/free-solid-svg-icons";
 import useLocalizedPush from "./useLocalizedPush";
+import useGiveOpinion from "./useGiveOpinion";
 
 export interface RoomAction {
     id: string;
@@ -18,8 +19,14 @@ export interface RoomAction {
 
 export default function useRoomActions() {
     const { _ } = useLingui();
-    const { roomId, isRoomEnded, getOutcomeByType } = useRoom();
+    const { roomId, isRoomEnded, getOutcomeByType, lastOutcome, participantId, participants, refetchMessages } = useRoom();
+    const { getGroupOpinions } = useGiveOpinion({
+        subjects: [lastOutcome],
+        participantId,
+    });
+    const [sendMessage, { loading: isSendingMessage }] = useSendRoomMessageMutation();
     const lastOpenedAssistantAt = useAppSelector((state) => state.room.lastOpenedAssistantAt);
+    const [nextStatementPressed, setNextStatementPressed] = useState(false);
     const { push } = useLocalizedPush();
     const actions: RoomAction[] = useMemo(() => {
         const newActions: RoomAction[] = [];
@@ -28,6 +35,8 @@ export default function useRoomActions() {
             return createdAt && dayjs(createdAt).isAfter(lastOpenedAssistantAt);
         };
         const shouldNotifyConsensus = shouldNotify(latestConsensusOutcome?.created_at);
+        const hasEveryoneVoted = getGroupOpinions(lastOutcome?.id).length === participants?.length;
+        const canSkipOutcome = hasEveryoneVoted && !nextStatementPressed;
 
         if (shouldNotifyConsensus) {
             newActions.push({
@@ -39,11 +48,37 @@ export default function useRoomActions() {
             });
         }
 
+        if (canSkipOutcome) {
+            newActions.push({
+                id: 'can-skip-outcome',
+                icon: faForward,
+                title: _(msg`Next statement`),
+                onClick: async () => {
+                    if (isSendingMessage) {
+                        return;
+                    }
+
+                    setNextStatementPressed(true);
+                    await sendMessage({
+                        variables: {
+                            roomId,
+                            content: _(msg`I would like to move on to the next statement.`),
+                            participantId,
+                            tags: 'next-statement',
+                        },
+                    });
+
+                    // TMP: temporary until Realtime has better performance
+                    refetchMessages();
+                },
+            });
+        }
+
         if (isRoomEnded) {
             newActions.push({
                 id: 'end-room',
                 icon: faDoorOpen,
-                title: _(msg`Leave room`),
+                title: _(msg`Leave conversation`),
                 onClick: () => {
                     push(`/evaluate/${roomId}`);
                 },
@@ -51,7 +86,12 @@ export default function useRoomActions() {
         }
 
         return newActions;
-    }, [getOutcomeByType, isRoomEnded, lastOpenedAssistantAt, _, push, roomId]);
+    }, [nextStatementPressed, getOutcomeByType, getGroupOpinions, lastOutcome?.id, participants?.length, isRoomEnded, lastOpenedAssistantAt, _, push, roomId, isSendingMessage, sendMessage, participantId, refetchMessages]);
+
+    // allow next statement again
+    useEffect(() => {
+        setNextStatementPressed(false);
+    }, [lastOutcome]);
 
     return {
         actions,
