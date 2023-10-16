00ba1d691d4e3e60629125de26c53a5beb98022e
Pepijn Verburg <pepijn.verburg@gmail.com>
2023-10-16 15:40:29 +0200

    chore(das): updated the home page texts.

--- projects/deliberation_at_scale/packages/frontend/app/[lang]/page.tsx
+++ projects/deliberation_at_scale/packages/frontend/app/[lang]/page.tsx
@@ -29,12 +29,14 @@ export default function Index() {
     return (
         <section id="main-scroll-container" className="p-2 flex flex-col gap-3 overflow-y-scroll pt-24">
             <motion.div layout>
-                <h1 className="mb-4 text-3xl font-medium"><Trans>Experience, Understand and Influence AI</Trans></h1>
+                <h1 className="mb-4 text-3xl font-medium"><Trans>Experience and influence AI! Welcome to a conversation about AI in your city.</Trans></h1>
                 <p className="mb-4 text-gray-600">
-                    <Trans>Welcome to our conversation about AI in Eindhoven.<br/><br/>
-                    We all want the <b>best for our city</b>. This is why we want to figure how AI can help.
-                    We believe it is important that <b>people steer</b> this development.
-                    Thank you for being part of this effort!</Trans>
+                    <Trans>
+                        We all want the <b>best for the places where we live</b>.
+                        This is why we want to figure how AI can help.
+                        We believe it is important that <b>people steer</b> this development.
+                        Thank you for being part of this effort!
+                    </Trans>
                 </p>
                 <p className="mb-4 text-gray-600"><Trans>Your experience on Common Ground will look like:</Trans></p>
                 <div className="grid sm:grid-cols-2 sm:grid-rows-2 gap-2 mb-4 text-green-700 text-lg font-medium text-center">
@@ -48,11 +50,11 @@ export default function Index() {
                     </div>
                     <div className="bg-green-50 p-4 py-6 rounded flex flex-col items-center gap-4">
                         <FontAwesomeIcon icon={faShieldHeart} size="2x" fixedWidth />
-                        <Trans><h2>AI moderators help create a <b>healthy conversation</b></h2></Trans>
+                        <Trans><h2>An AI moderator may <b>help you in your conversation</b></h2></Trans>
                     </div>
                     <div className="bg-green-50 p-4 py-6 rounded flex flex-col items-center gap-4">
                         <FontAwesomeIcon icon={faLightbulb} size="2x" fixedWidth />
-                        <Trans><h2><b>Learn</b> from eachother and <b>contribute</b> to the 040 AI manifesto</h2></Trans>
+                        <Trans><h2><b>Learn</b> from eachother and <b>contribute</b>!</h2></Trans>
                     </div>
                 </div>
                 {!isLoggedIn && (
